cea8ddd1c3499defd02931575142112d9c6adc3d
fUNNY-fnaf <itsafeat@gmail.com>
2021-10-27 00:24:28 +0200

    Buoyancy virker fandme!
    Det skal squ fejres

--- Godot Project/Scripts/EntityScripts/Buoyancy.gd
+++ Godot Project/Scripts/EntityScripts/Buoyancy.gd
@@ -1,16 +1,16 @@
-extends RigidBody
+extends Spatial
 
-onready var water_node = get_parent().get_node("Wasser")
+onready var water_node = get_parent().get_parent().get_node("Water")
 onready var water_mat  = water_node.mesh.surface_get_material(0)
+onready var ship_node = get_parent()
 
 var time = 0.0
+var submerged_depth = 0.0
 
-var _WAVE_SPEED = 0
 var _WAVE_SIZE  = 0
 var _HEIGHT     = 0
 
-func _ready():
-	#_WAVE_SPEED = water_mat.get_shader_param("_WAVE_SPEED")
+func _ready() -> void:
 	set_process(true)
 
 
@@ -33,12 +33,13 @@ func get_height(position : Vector2) -> float:
 
 func apply_buoyancy(point : Vector3) -> void:
 	var depth = get_height(Vector2(point.x, point.z)) - point.y
-	if depth > 0:
-		add_force(Vector3.UP * depth, point - self.transform.origin)
+	if depth > submerged_depth:
+		ship_node.add_central_force(Vector3(0, depth, 0))
+		ship_node.add_force(Vector3(0, depth/4, 0), self.transform.origin)
 
 
-func _process(delta):
-	time += delta
-	water_mat.set_shader_param("_TIME", time)
-	
-	apply_buoyancy(self.transform.origin)
+func _process(delta) -> void:
+	if not get_tree().paused:
+		time += delta
+		water_mat.set_shader_param("_TIME", time)
+		apply_buoyancy(self.global_transform.origin)
